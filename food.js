7588ba86c7cea859f653e80fd58ef4313e758a75
Dino Himaj <dinohimaj@rocketmail.com>
2022-12-28 15:39:24 +0100

    finished game mechanics/ added retry option

--- food.js
+++ food.js
@@ -1,12 +1,13 @@
+import { randomGridPosition } from './grid.js';
 import { onSnake, expandSnake } from './snake.js';
 
-let food = { x: 10, y: 1 };
-const EXPANSION_RATE = 1; //how much snake grows when it eats food
+let food = getRandomFoodPosition();
+const EXPANSION_RATE = 5; //how much snake grows when it eats food
 
 export function update() {
   if (onSnake(food)) {
     expandSnake(EXPANSION_RATE);
-    food = { x: 20, y: 10 };
+    food = getRandomFoodPosition();
   }
 }
 
@@ -16,4 +17,13 @@ export function draw(gameBoard) {
   foodElement.style.gridColumnStart = food.x;
   foodElement.classList.add('food');
   gameBoard.appendChild(foodElement);
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition;
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition();
+  }
+
+  return newFoodPosition;
 }
--- game.js
+++ game.js
@@ -2,14 +2,24 @@ import {
   update as updateSnake,
   draw as drawSnake,
   SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
 } from './snake.js';
-
 import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 
 let lastRenderTime = 0;
+let gameOver = false;
 const gameBoard = document.getElementById('game-board');
 
 function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press Ok to restart')) {
+      window.location = '/'; //refresh page
+    }
+    return; //if cancel is pressed => exit the loop
+  }
+
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
@@ -25,10 +35,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = ''; //clearing previous snake pieces
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
